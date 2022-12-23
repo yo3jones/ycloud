b27c7d1802912fb9ae00b3692beda9ff9f213d62
Chris Jones <chris@Chriss-MacBook-Air.local>
2022-12-23 09:15:43 -0800

    add example of using a .env file for managing variables

--- yorg/README.md
+++ yorg/README.md
@@ -6,6 +6,7 @@
 
 The `dev-init.sh` will do the following:
 
+- copies the example.env to .env if it doesn't exist
 - build the project using cmake
 - symlink the compile_commands.json so lsp functionality will work
 
--- yorg/scripts/dev-init.sh
+++ yorg/scripts/dev-init.sh
@@ -1,5 +1,11 @@
 #! /bin/bash
 
+# Copy example.env to .env if it doesn't exist
+if [ ! -f .env ]
+then
+  cp example.env .env
+fi
+
 cmake -S . -B build \
   && cmake --build build \
   && ln -s -f build/compile_commands.json compile_commands.json
