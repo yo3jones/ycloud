fa3483e5cc52dec551575c524d744c87ba3219ba
Chris Jones <chris@yo3.io>
2023-01-05 11:21:59 -0800

    add a semaphore util

---
+++ yutil/test/semaphore_test.cpp
@@ -0,0 +1,30 @@
+#include <pthread.h>
+#include <unistd.h>
+#include "../include/yutil.h"
+#include "gtest/gtest.h"
+
+#define SLEEP             1
+#define THREAD_COUNT      10
+#define SEMAPHORE_DESIRED 5
+
+void* hmm(void* p) {
+  yutil::Semaphore* semaphore = reinterpret_cast<yutil::Semaphore*>(p);
+  semaphore->aquire();
+  sleep(SLEEP);
+  semaphore->release();
+  return NULL;
+}
+
+TEST(SemaphoreTest, test) {
+  pthread_t        threads[THREAD_COUNT];
+  yutil::Semaphore semaphore(SEMAPHORE_DESIRED);
+
+  for (int i = 0; i < THREAD_COUNT; i++) {
+    pthread_create(&threads[i], NULL, hmm, reinterpret_cast<void*>(&semaphore));
+  }
+
+  for (int i = 0; i < THREAD_COUNT; i++) {
+    void* status;
+    pthread_join(threads[i], &status);
+  }
+}
